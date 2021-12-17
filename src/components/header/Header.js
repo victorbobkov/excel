781929d3a4eddb371a0d3164e3d31316807c29ea
victor bobkov <victorbobkov@gmail.com>
2021-12-17 05:15:06 +0300

    adding and deleting listeners

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,23 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 
 export class Header extends ExcelComponent {
-  static className = 'excel__header'
+   static className = 'excel__header'
 
-  toHTML() {
-    return `
-    <input type="text" class="input" value="Новая таблица" />
-
-            <div>
-
-                <div class="button">
-                    <i class="material-icons">delete</i>
-                </div>
-
-                <div class="button">
-                    <i class="material-icons">logout</i>
-                </div>
-
-            </div>
-    `
-  }
+   toHTML() {
+     return `
+       <input type="text" class="input" value="Новая таблица"/>
+  
+       <div>
+  
+         <div class="button">
+           <i class="material-icons">delete</i>
+         </div>
+  
+         <div class="button">
+           <i class="material-icons">logout</i>
+         </div>
+  
+       </div>
+   `
+   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -19,8 +19,12 @@ class Dom {
     return this
   }
 
-  on() {
+  on(eventType, callback) {
+    this.$el.addEventListener(eventType, callback)
+  }
 
+  off(eventType, callback) {
+    this.$el.removeEventListener(eventType, callback)
   }
 
   // Element
